b32f2551c73c1b13fd0d144913f3518102f15655
yaruo69 <shusaku69_1998@icloud.com>
2025-03-13 18:58:59 +0900

    初期画面の完成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
-import { useEffect } from 'react';
-import './App.css';
-import { getAllPokemon } from './utils/pokemon';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
+import Card from "./components/Card/Card";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon/";
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+
   // useEffectは、第二引数に[]を設定することで、初回レンダリングの時のみに実行。
   useEffect(() => {
     // asyncをつけることで、その関数が非同期処理になる。
@@ -12,12 +16,45 @@ function App() {
       // 全てのポケモンデータを取得
       // awaitは、Promiseが解決するのを待ってから、次の処理を実行する形にできる。
       let res = await getAllPokemon(initialURL);
-      console.log(res);
-    }
-    fetchPokemonData()
-  }, [])
+      // 各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
+      setLoading(false);
+    };
+    fetchPokemonData();
+  }, []);
 
-  return <div className="App"></div>;
+  const loadPokemon = async (data) => {
+    // Promise.allは、複数の非同期処理(Promise)を並列して実行し、すべての完了を待つ関数。
+    // 成功すると、結果を配列で返す。
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
+
+  return (
+    <>
+      <Navbar />
+      <div className="App">
+        {loading ? (
+          <h1>ロード中・・・</h1>
+        ) : (
+          <>
+            <div className="pokemonCardContainer">
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />;
+              })}
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -1,7 +1,19 @@
+// 全ポケモンのデータを取得
 export const getAllPokemon = (url) => {
-    return new Promise((resolve, reject) => {
-        // fetchは、APIからデータを取得する関数
-        // Promiseが返ってくる
-        fetch(url).then((res) => res.json()).then((data) => resolve(data));
-    })
-}
+  return new Promise((resolve, reject) => {
+    // fetchは、APIからデータを取得する関数
+    // Promiseが返ってくる
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => resolve(data));
+  });
+};
+
+// 各ポケモンの詳細情報を取得
+export const getPokemon = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => resolve(data));
+  });
+};
